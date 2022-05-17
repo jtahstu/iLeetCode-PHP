a9993d5a9e9f22dce84e46aea3155ac2cc878505
jtahstu <root@jtahstu.com>
2022-05-17 23:44:15 +0800

    feature: 6068. 毯子覆盖的最多白色砖块数

--- Contest/biweekly 78 - 6068. 毯子覆盖的最多白色砖块数.php
+++ Contest/biweekly 78 - 6068. 毯子覆盖的最多白色砖块数.php
@@ -17,6 +17,53 @@ class Solution
      */
     function maximumWhiteTiles($tiles, $carpetLen)
     {
+        //先给tiles排序
+        $sort_arr = [];
+        foreach ($tiles as $tile) {
+            $sort_arr[] = $tile[0];
+        }
+        array_multisort($sort_arr, SORT_NUMERIC, SORT_ASC, $tiles);
+        $n = count($tiles);
+        $l = $r = $max = $sum = 0;
+        while ($l <= $r && $r < $n) {
+            $left_point = $tiles[$l][0];
+            $right_point = $left_point + $carpetLen - 1;
+            //把r完全覆盖了, 就把r长度加上
+            if ($right_point >= $tiles[$r][1]) {
+                $sum += $tiles[$r][1] - $tiles[$r][0] + 1;
+                $r++;
+                $max = max($max, $sum);
+            } else {
+                //毯子只覆盖到r的部分
+                if ($right_point >= $tiles[$r][0]) {
+                    $max = max($max, $sum + $right_point - $tiles[$r][0] + 1);
+                }
+                //l处理结束, 移到下一个tile上, sum也去掉当前l毯子的长度
+                $sum -= $tiles[$l][1] - $tiles[$l][0] + 1;
+                $l++;
+            }
+        }
+        return $max;
+    }
+}
+
+/**
+ * 执行用时：420 ms, 在所有 PHP 提交中击败了100.00%的用户
+ * 内存消耗：77.8 MB, 在所有 PHP 提交中击败了100.00%的用户
+ * 通过测试用例：51 / 51
+ */
+
+//比赛时代码
+class Solution2
+{
+
+    /**
+     * @param Integer[][] $tiles
+     * @param Integer $carpetLen
+     * @return Integer
+     */
+    function maximumWhiteTiles($tiles, $carpetLen)
+    {
 //        $carpetLen++;
         $dp = [[0]];
         $sort_arr = [];
@@ -46,7 +93,7 @@ class Solution
 //                    $res[$j] = $dp[$i][$j];
 //                }
                 if (isset($dp[$i - 1][$j - $carpetLen])) {
-                    $dp[$i][$j] = max($j-$tiles[$i][0] + 1, $dp[$i][$j - 1] + $dp[$i - 1][$j - $carpetLen]);
+                    $dp[$i][$j] = max($j - $tiles[$i][0] + 1, $dp[$i][$j - 1] + $dp[$i - 1][$j - $carpetLen]);
                 } else {
 
                 }
@@ -62,9 +109,9 @@ class Solution
     }
 }
 
-//var_dump((new Solution)->maximumWhiteTiles([[1, 5], [10, 11], [12, 18], [20, 25], [30, 32]], 10));
-//var_dump((new Solution)->maximumWhiteTiles([[1,5],[10,11],[12,18],[20,25],[30,32]], 10));
-//var_dump((new Solution)->maximumWhiteTiles([[10, 11], [1, 1]], 2));
-var_dump((new Solution)->maximumWhiteTiles([[8051,8057],[8074,8089],[7994,7995],[7969,7987],[8013,8020],[8123,8139],[7930,7950],[8096,8104],[7917,7925],[8027,8035],[8003,8011]],9854));
+var_dump((new Solution)->maximumWhiteTiles([[1, 5], [10, 11], [12, 18], [20, 25], [30, 32]], 10));
+var_dump((new Solution)->maximumWhiteTiles([[1, 5], [10, 11], [12, 18], [20, 25], [30, 32]], 10));
+var_dump((new Solution)->maximumWhiteTiles([[10, 11], [1, 1]], 2));
+var_dump((new Solution)->maximumWhiteTiles([[8051, 8057], [8074, 8089], [7994, 7995], [7969, 7987], [8013, 8020], [8123, 8139], [7930, 7950], [8096, 8104], [7917, 7925], [8027, 8035], [8003, 8011]], 9854));
 
 //GG
